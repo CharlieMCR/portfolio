86ccde476740c7ea0d316e12085566c1a0f5293a
CharlieMCR <charlieheesom@gmail.com>
2014-11-11 17:35:23 +0000

    useless small changes

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 require_once("inc/config.php");
 
 include("inc/header.php") ?>
-         
-        
-         
+
         <?php include("projects.php");?>
-         
 
 
 <?php include 'inc/footer.php'; ?>
