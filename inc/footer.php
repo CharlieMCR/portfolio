7b3f9feff8ece255dd3d1c115653a05f6227a989
CharlieMCR <charlieheesom@gmail.com>
2014-11-14 17:50:49 +0000

    added analytics

--- inc/footer.php
+++ inc/footer.php
@@ -23,10 +23,17 @@
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
     
-    <script src="<?php echo BASE_URL; ?>js/app.js" type="text/javascript">
-        
+    <script src="<?php echo BASE_URL; ?>js/app.js" type="text/javascript"></script>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    </script>
+  ga('create', 'UA-56759332-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     
 </body>
 
